048b65312266bdcc403458ceec0136ff3bd843ad
Chokchai213 <chokchai.bis@gmail.com>
2023-09-23 00:09:24 +0700

    rebuild package , mock frontend components

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,28 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import CollapsibleExample from './components/Navbar';
+import IndividualIntervalsExample from './components/Carousel';
 function App() {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <CollapsibleExample/>
+    <h1>
+      <IndividualIntervalsExample/>
+    </h1>
     </div>
   );
 }
-
-
-
 
 export default App;
