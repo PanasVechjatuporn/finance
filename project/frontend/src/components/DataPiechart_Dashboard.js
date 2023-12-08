d0a30832ee4d1d21359692aef6c0fa646f73493e
Taro <momo.taro9071@gmail.com>
2023-12-08 16:26:16 +0700

    fix pie chart for new data format

--- project/frontend/src/components/DataPiechart_Dashboard.js
+++ project/frontend/src/components/DataPiechart_Dashboard.js
@@ -8,14 +8,23 @@ export const Piechart = ({ data, startDate, endDate }) => {
         return objDate >= start && objDate <= end;
     });
 
-    const sumIncome = dataArray.reduce((sum, obj) => sum + parseInt(obj.income), 0);
-    const sumInvest = dataArray.reduce((sum, obj) => sum + parseInt(obj.investment), 0);
-    const sumFixedExpense = dataArray.reduce((sum, obj) => sum + parseInt(obj.expense.fixed_expense), 0);
-    const sumVariableExpense = dataArray.reduce((sum, obj) => sum + parseInt(obj.expense.variable_expense), 0);
+    let sum_income = 0;
+    let sum_investment = 0;
+    let sum_fixed_expense = dataArray.reduce((sum, record) => sum + parseInt(record.expense.variable_expense), 0);
+    let sum_variable_expense = dataArray.reduce((sum, record) => sum + parseInt(record.expense.variable_expense), 0);
+    
+    dataArray.forEach(record => {
+        const income_list = record.income;
+        const investment_list = record.investment;        
+        
+        sum_income += parseInt(income_list.reduce((sum, income) => sum + parseInt(income.amount),0));
+        sum_investment += parseInt(investment_list.reduce((sum, investment) => sum + parseInt(investment.amount),0));
+        
+    });
 
-    const percentInvest = Math.round((sumInvest / sumIncome) * 100)
-    const percentFixedExpense = Math.round((sumFixedExpense / sumIncome) * 100)
-    const percentVariableExpense = Math.round((sumVariableExpense / sumIncome) * 100)
+    const percentInvest = Math.round((sum_investment / sum_income) * 100)
+    const percentFixedExpense = Math.round((sum_fixed_expense / sum_income) * 100)
+    const percentVariableExpense = Math.round((sum_variable_expense / sum_income) * 100)
 
     data = [
         { 'value': percentInvest, 'label': 'Investment' },
--- project/frontend/src/components/DataTable_Dashboard.js
+++ project/frontend/src/components/DataTable_Dashboard.js
@@ -1,156 +1,143 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import Box from "@mui/material/Box";
-import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
-
-
-export default function CollapsibleTable({ data }) {
-  const dataArray = data.filter(obj => {
-    const { date, income, expense, investment } = obj;
-    return obj
-  });
-  // console.log("LOL: ", dataArray[0])
-
-  // const rows_real = dataArray.forEach(element => {
-  // console.log(element.date)
-  // console.log(element.income)
-  // console.log(element.expense)
-  // console.log(element.investment)
-  // });
-
-  const mapping = [dataArray.map(function (item) {
-    return createData(item.date, item.income, item.expense, item.investment)
-    // console.log(parseInt(item.income.reduce((sum, item) => parseInt(sum) + parseInt(item.amount), 0)))
-  })]
-
-  Row.propTypes = {
-    row: PropTypes.shape({
-      SumOfIncome: PropTypes.number.isRequired,
-      SumOfInvestment: PropTypes.number.isRequired,
-      SumOfExpenses: PropTypes.number.isRequired,
-      history: PropTypes.arrayOf(
-        PropTypes.shape({
-          amount: PropTypes.number.isRequired,
-          customerId: PropTypes.string.isRequired,
-          date: PropTypes.string.isRequired,
-        })
-      ).isRequired,
-      name: PropTypes.string.isRequired,
-      price: PropTypes.number.isRequired,
-      // protein: PropTypes.number.isRequired,
-    }).isRequired,
+function createData(name, calories, fat, carbs, protein, price) {
+  return {
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+    price,
+    history: [
+      {
+        date: '2020-01-05',
+        customerId: '11091700',
+        amount: 3,
+      },
+      {
+        date: '2020-01-02',
+        customerId: 'Anonymous',
+        amount: 1,
+      },
+    ],
   };
+}
 
-  const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0, 3.99),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
-    createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
-    createData("Cupcake", 305, 3.7, 67, 4.3, 2.5),
-    createData("Gingerbread", 356, 16.0, 49, 3.9, 1.5),
-  ];
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
 
-  function createData(date, income, expense, investment) {
-    return {
-      date,
-      totalIncomeAmount,
-      totalExpensesAmount,
-      totalInvestmentAmount,
-      history: [income.forEach(element => {
-        return (
-          console.log('hello')
-        )
-      })]
-    };
-  }
-
-  function Row(props) {
-    const { row } = props;
-    const [open, setOpen] = React.useState(false);
-
-    return (
-      <React.Fragment>
-        <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
-          <TableCell>
-            <IconButton
-              aria-label="expand row"
-              size="small"
-              onClick={() => setOpen(!open)}
-            >
-              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {row.name}
-          </TableCell>
-          <TableCell align="right">{row.calories}</TableCell>
-          <TableCell align="right">{row.fat}</TableCell>
-          <TableCell align="right">{row.carbs}</TableCell>
-          <TableCell align="right">{row.protein}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <Box sx={{ margin: 1 }}>
-                <Typography variant="h6" gutterBottom component="div">
-                  History
-                </Typography>
-                <Table size="small" aria-label="purchases">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Date</TableCell>
-                      <TableCell>Customer</TableCell>
-                      <TableCell align="right">Amount</TableCell>
-                      <TableCell align="right">Total price ($)</TableCell>
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell align="right">{row.calories}</TableCell>
+        <TableCell align="right">{row.fat}</TableCell>
+        <TableCell align="right">{row.carbs}</TableCell>
+        <TableCell align="right">{row.protein}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                History
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Customer</TableCell>
+                    <TableCell align="right">Amount</TableCell>
+                    <TableCell align="right">Total price ($)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.date}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.date}
+                      </TableCell>
+                      <TableCell>{historyRow.customerId}</TableCell>
+                      <TableCell align="right">{historyRow.amount}</TableCell>
+                      <TableCell align="right">
+                        {Math.round(historyRow.amount * row.price * 100) / 100}
+                      </TableCell>
                     </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {row.history.map((historyRow) => (
-                      <TableRow key={historyRow.date}>
-                        <TableCell component="th" scope="row">
-                          {historyRow.date}
-                        </TableCell>
-                        <TableCell>{historyRow.customerId}</TableCell>
-                        <TableCell align="right">{historyRow.amount}</TableCell>
-                        <TableCell align="right">
-                          {Math.round(historyRow.amount * row.price * 100) / 100}
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </Box>
-            </Collapse>
-          </TableCell>
-        </TableRow>
-      </React.Fragment>
-    );
-  }
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
 
+Row.propTypes = {
+  row: PropTypes.shape({
+    calories: PropTypes.number.isRequired,
+    carbs: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    history: PropTypes.arrayOf(
+      PropTypes.shape({
+        amount: PropTypes.number.isRequired,
+        customerId: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    protein: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
+  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
+  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
+  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
+];
 
+export default function CollapsibleTable() {
   return (
     <TableContainer component={Paper}>
       <Table aria-label="collapsible table">
         <TableHead>
           <TableRow>
             <TableCell />
-            <TableCell>Date</TableCell>
-            <TableCell align="right">Sum of income</TableCell>
-            <TableCell align="right">Sum of Investment&nbsp;</TableCell>
-            <TableCell align="right">Sum of Expenses&nbsp;</TableCell>
-            {/* <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
