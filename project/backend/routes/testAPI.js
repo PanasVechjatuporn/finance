b9aed9976b75e4328554b9dd1cf433424f222c1f
Chokchai213 <global>
2024-02-23 20:17:11 +0700

    Delete backend

--- project/backend/routes/testAPI.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/",function(req,res,next){
-    //res.send("API in working properly");
-    res.json({"temp":["temp1","temp2"]}); //test sending json data
-});
-
-module.exports = router;
