bd63ce5a45b1ab0c65ca6cbf8bfc33bf48646755
Chokchai213 <global>
2024-02-26 05:32:32 +0700

    Update firebaseRoute.js

--- project/backend/src/routes/firebaseRoute.js
+++ project/backend/src/routes/firebaseRoute.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express();
 const firebaseAuth = require('../controllers/firebaseAuth');
 
-router.get('/test', (req, res) => {
-    res.json({ message: 'This is a sample JSON response' });
-});
-
-// router.post('/signup', firebaseAuth.signup());
 router.post('/signup', async function (req, res) {
     var user = await firebaseAuth.signup(req, res)
 });
@@ -14,10 +9,5 @@ router.post('/signup', async function (req, res) {
 router.post('/signin', async function (req, res) {
     var user = await firebaseAuth.signin(req, res)
 });
-
-// router.post('/signout', firebaseAuth.signout());
-
-// router.post('/signin/checkuid', firebaseAuth.checkuid());
-
 
 module.exports = router;
