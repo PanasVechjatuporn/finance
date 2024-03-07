ef88847f5996d3079911293560eaa3b3edb151c5
Chokchai213 <global>
2024-03-07 17:23:42 +0700

    add protected route require user state in localStorage to login

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,5 +1,5 @@
 import "App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard } from "pages/dashboard";
 import { Home } from "pages/homepage";
 import { EditFormPage } from "pages/editpages_form";
@@ -9,6 +9,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { LoginWithLocalData } from './store/UserSlice';
 import axios from 'axios';
 const baseURL = "http://localhost:8000";
+function RequireAuth({ children, redirectTo }) {
+  const localUser = JSON.parse(localStorage.getItem('userData'))
+  alert("Please Login")
+  if (!localUser) {
+    return <Navigate to={redirectTo} />
+  }
+  return localUser.isLogIn ? children : <Navigate to={redirectTo} />;
+}
 function App() {
   const userStore = useSelector(state => state.userStore)
   const dispatch = useDispatch()
@@ -32,8 +40,22 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/edit-form" element={<EditFormPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuth redirectTo="/">
+              {<Dashboard />}
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/Goal-Based"
+          element={
+            <RequireAuth redirectTo="/">
+              {<GoalBased />}
+            </RequireAuth>
+          }
+        />
         <Route path="/Goal-Based" element={<GoalBased />} />
         <Route path="/Goal-Based/reduce-tax-goal" element={<NewTaxGoal />} />
       </Routes>
