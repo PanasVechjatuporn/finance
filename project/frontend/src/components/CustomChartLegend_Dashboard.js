d60d21fd6903b308b906f35d98c212151bf02f3b
Chokchai213 <chokchai.bis@gmail.com>
2024-03-24 00:15:12 +0700

    add custom chart legend

---
+++ project/frontend/src/components/CustomChartLegend_Dashboard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import { Box } from '@mui/material';
+import Grid from '@mui/material/Grid';
+export default function CustomChartLegend({ data }) {
+    return (<>
+        {
+            data.map((data, index) => (
+                <Grid container spacing={2} key={index+data.label+"grid"}>
+                    <Grid item key={index+data.label+"grid-box-grid"}>
+                        <Box
+                            sx={{
+                                width: 16,
+                                height: 16,
+                                borderRadius: 1,
+                                bgcolor: data.color,
+                                marginTop:1
+                            }}
+                        />
+                    </Grid>
+                    <Grid item key={index+"-"+data.label+"-grid-typography-grid"}>
+                        <Typography sx={{
+                                marginTop:1
+                            }}>{data.label}</Typography>
+                    </Grid>
+                </Grid>
+            ))
+        }
+    </>)
+}
--- project/frontend/src/components/PieChart_Dashboard.js
+++ project/frontend/src/components/PieChart_Dashboard.js
@@ -1,31 +1,144 @@
 import React, { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import { Box, Typography } from "@mui/material";
-import { PieChart } from "@mui/x-charts/PieChart";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-const pieChartsParams = {
-    series: [
-        {
-            data: [{ value: 5 }, { value: 10 }],
-            label: "Series 1",
-            highlighted: { additionalRadius: 10 },
-        },
-    ],
-    height: 400,
-    margin: { top: 50, bottom: 50 },
-};
-
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { Pie } from "react-chartjs-2";
+import {Chart, ArcElement} from 'chart.js'
+Chart.register(ArcElement);
+const taxableIncome = [
+    {
+        name: "เงินได้ประเภทที่ 1",
+        category: 1,
+        label: "เงินเดือน",
+    },
+    {
+        name: "เงินได้ประเภทที่ 2",
+        category: 2,
+        label: "ค่าจ้างทั่วไป",
+    },
+    {
+        name: "เงินได้ประเภทที่ 3",
+        category: 3,
+        label: "ค่าลิขสิทธิ์และทรัพย์สินทางปัญญา",
+        subcategory: [
+            {
+                subcategorylabel:
+                    "ค่าลิขสิทธิ์/ค่าลิขสิทธิ์และทรัพย์สินทางปัญญา/ค่าGoodwill",
+                subcategorycategory: 1,
+                subcategoryinfo: "หักแบบเหมา 50% แต่ไม่เกิน 100,000 บาท หรือหักตามจริง",
+            },
+            {
+                subcategorylabel: "เงินปีและเงินรายปีจากนิติกรรมหรือคำพิพากษาของศาล",
+                subcategorycategory: 2,
+                subcategoryinfo: "-",
+            },
+        ],
+    },
+    {
+        name: "เงินได้ประเภทที่ 4",
+        category: 4,
+        label: "ดอกเบี้ย/เงินปันผล/ผลประโยชน์จากการลงทุน",
+    },
+    {
+        name: "เงินได้ประเภทที่ 5",
+        category: 5,
+        label: "เงินได้จากการให้เช่าทรัพย์สิน",
+        subcategory: [
+            {
+                subcategorylabel: "ค่าเช่าบ้าน/อาคาร/ตึก/สิ่งปลูกสร้าง/แพ",
+                subcategorycategory: 1,
+                subcategoryinfo: "หักแบบเหมา 30% หรือหักตามจริง",
+            },
+            {
+                subcategorylabel: "ค่าเช่าที่ดินเกษตรกรรม",
+                subcategorycategory: 2,
+                subcategoryinfo: "หักแบบเหมา 20% หรือหักตามจริง",
+            },
+            {
+                subcategorylabel: "ค่าเช่าที่ดินไม่ใช้ในเกษตรกรรม",
+                subcategorycategory: 3,
+                subcategoryinfo: "หักแบบเหมา 15% หรือหักตามจริง",
+            },
+            {
+                subcategorylabel: "ค่าเช่ายานพาหนะ",
+                subcategorycategory: 4,
+                subcategoryinfo: "หักแบบเหมา 30% หรือหักตามจริง",
+            },
+            {
+                subcategorylabel: "ค่าเช่าอื่นๆ",
+                subcategorycategory: 5,
+                subcategoryinfo: "หักแบบเหมา 10% หรือหักตามจริง",
+            },
+        ],
+    },
+    {
+        name: "เงินได้ประเภทที่ 6",
+        category: 6,
+        label: "เงินได้จากวิชาชีพอิสระ",
+        subcategory: [
+            {
+                subcategorylabel: "การประกอบโรคศิลปะ",
+                subcategorycategory: 1,
+                subcategoryinfo: "หักแบบเหมา 60% หรือหักตามจริง",
+            },
+            {
+                subcategorylabel: "กฎหมาย/วิศวกรรม/สถาปัตยกรรม/บัญชี/ประณีตศิลปกรรม",
+                subcategorycategory: 2,
+                subcategoryinfo: "หักแบบเหมา 30% หรือหักตามจริง",
+            },
+        ],
+    },
+    {
+        name: "เงินได้ประเภทที่ 7",
+        category: 7,
+        label: "เงินได้จากการรับเหมา (ก่อสร้าง/รับผลิตสินค้า)",
+    },
+];
+const Data = [
+    {
+      id: 1,
+      year: 2016,
+      userGain: 80000,
+      userLost: 823
+    },
+    {
+      id: 2,
+      year: 2017,
+      userGain: 45677,
+      userLost: 345
+    },
+    {
+      id: 3,
+      year: 2018,
+      userGain: 78888,
+      userLost: 555
+    },
+    {
+      id: 4,
+      year: 2019,
+      userGain: 90000,
+      userLost: 4555
+    },
+    {
+      id: 5,
+      year: 2020,
+      userGain: 4300,
+      userLost: 234
+    }
+  ];
 export default function PieChartComponent({ userData }) {
     const [highlighted] = useState("item");
     const [faded] = useState("series");
-    const [currentYear, setCurrentYear] = useState(userData ? userData[0].year : null);
-    const [pieExpenseInvestmentParams, setPieExpenseInvestmentParams] = useState(null);
+    const [currentYear, setCurrentYear] = useState(
+        userData ? userData[0].year : null
+    );
+    const [pieExpenseInvestmentParams, setPieExpenseInvestmentParams] =
+        useState(null);
     const [pieIncomeParams, setPieIncomeParams] = useState(null);
+    const [incomePieData, setIncomePieData] = useState(null);
     useEffect(() => {
         if (userData && !currentYear) {
             setCurrentYear(userData[0].year)
@@ -33,49 +146,94 @@ export default function PieChartComponent({ userData }) {
             if (userData[0].data.length > 0) {
                 const currentYearData = userData.filter(item => item.year === currentYear)[0].data;
                 let yearlyIncome = 0;
-                let incomePieData = []
-                currentYearData.forEach(({ incomeData, expenseData, investmentData }) => {
-                    incomeData.forEach((incomeSource) => {
-                        const existingEntry = incomePieData.find(entry => entry.type === incomeSource.type);
-                        if (existingEntry) {
-                            existingEntry.value += parseFloat(incomeSource.amount);
-                        } else {
-                            incomePieData.push({
-                                type: incomeSource.type,
-                                value: parseFloat(incomeSource.amount)
-                            });
-                        }
-                        yearlyIncome += parseFloat(incomeSource.amount);
-                    })
-                });
+                let incomePieData = [];
+                let expensePieData = [];
+                currentYearData.forEach(
+                    ({ incomeData, expenseData, investmentData }) => {
+                        //incomeData
+                        incomeData.forEach((incomeSource) => {
+                            const existingEntry = incomePieData.find(
+                                (entry) => entry.type === incomeSource.type
+                            );
+                            const incomeTypeLabel = taxableIncome.find(
+                                (entry) => entry.category === incomeSource.type
+                            ).label;
+                            if (existingEntry) {
+                                existingEntry.value += parseFloat(incomeSource.amount);
+                            } else {
+                                incomePieData.push({
+                                    type: incomeSource.type,
+                                    value: parseFloat(incomeSource.amount),
+                                    label: incomeTypeLabel,
+                                });
+                            }
+                            yearlyIncome += parseFloat(incomeSource.amount);
+                        });
+                        console.log('incomePieData :: ',incomePieData)
+                        //expenseData
+                        expenseData.forEach((expenseSource) => {
+                            const existingEntry = expensePieData.find(
+                                (entry) => entry.type === expenseData.type
+                            );
+                            if (existingEntry) {
+                                existingEntry.value += parseFloat(expenseSource.amount);
+                            } else {
+                                expensePieData.push({});
+                            }
+                        });
+                        //investmentData
+                    }
+                );
+                // {
+                //     labels: Data.map((data) => data.year), 
+                //     datasets: [
+                //       {
+                //         label: "Users Gained ",
+                //         data: Data.map((data) => data.userGain),
+                //         backgroundColor: [
+                //           "rgba(75,192,192,1)",
+                //           &quot;#ecf0f1",
+                //           "#50AF95",
+                //           "#f3ba2f",
+                //           "#2a71d0"
+                //         ],
+                //         borderColor: "black",
+                //         borderWidth: 2
+                //       }
+                //     ]
+                //   }
                 setPieIncomeParams({
-                    series: [
-                        {
-                            data: incomePieData,
-                            label: "Series 1",
-                            highlighted: { additionalRadius: 10 },
-                        },
-                    ],
-                    height: 400,
-                    margin: { top: 50, bottom: 50 },
-                })
-                console.log('incomePieData :: ', incomePieData)
-                console.log('yearlyIncome :: ', yearlyIncome)
+                    labels: Data.map((data) => data.year), 
+                    datasets: [
+                      {
+                        label: "Users Gained ",
+                        data: Data.map((data) => data.userGain),
+                        backgroundColor: [
+                          "rgba(75,192,192,1)",
+                          "#50AF95",
+                          "#f3ba2f",
+                          "#2a71d0"
+                        ],
+                        borderColor: "black",
+                        borderWidth: 2
+                      }
+                    ]
+                  })
+                // setPieIncomeParams({
+                //     series: [
+                //         {
+                //             data: incomePieData,
+                //             highlighted: { additionalRadius: 10 },
+                //         },
+                //     ],
+                //     height: 400,
+                //     paddingAngle: 5,
+                //     cornerRadius: 5,
+                //     margin: { top: 50, bottom: 50 },
+                // });
             }
         }
-        // console.log('userData[2024] :: ',userData[0].data)
-        // setPieChartsParams({
-        //     series: [
-        //         {
-        //             data: [{ value: 5 }, { value: 10 }],
-        //             label: "Series 1",
-        //             highlighted: { additionalRadius: 10 },
-        //         },
-        //     ],
-        //     height: 400,
-        //     margin: { top: 50, bottom: 50 },
-        // })
-    }, [userData, currentYear])
+    }, [userData, currentYear]);
 
     return (
         <Container maxWidth="md">
@@ -88,65 +246,53 @@ export default function PieChartComponent({ userData }) {
                     padding: 4,
                 }}
             >
-                <Stack
-                    direction={{ xs: "column", xl: "row" }}
-                    spacing={1}
-                    sx={{ width: "100%" }}
-                >
-                    <Box sx={{ flexGrow: 1 }}>
-                        <Typography>สรุปรายรับ</Typography>
-                        {pieIncomeParams ? (<PieChart
-                            {...pieIncomeParams}
-                            series={pieIncomeParams.series.map((series) => ({
-                                ...series,
-                                highlightScope: {
-                                    highlighted,
-                                    faded,
-                                },
-                            }))}
-                            onClick={(data, index) => {
+                <Typography>สรุปรายรับ ปี {currentYear}</Typography>
+                {pieIncomeParams ? (
+                    <div className="chart-container">
+                        <h2 style={{ textAlign: "center" }}>Pie Chart</h2>
+                        <Pie
+                            data={pieIncomeParams}
+                            options={{
+                                plugins: {
+                                    title: {
+                                        display: true,
+                                        text: "Users Gained between 2016-2020"
+                                    }
+                                }
+                            }}
+                        />
+                    </div>
+                ) : (
+                    <>No Income Data</>
+                )}
+                <Typography>สรุปรายจ่าย/เงินลงทุน</Typography>
+                {pieExpenseInvestmentParams ? (
+                    <div>
 
-                            }}
-                        />) : <>No Income Data</>}
-
-                    </Box>
-                    <Box sx={{ flexGrow: 1 }}>
-                        <Typography>สรุปรายจ่าย/เงินลงทุน</Typography>
-                        {pieExpenseInvestmentParams ? (<PieChart
-                            {...pieExpenseInvestmentParams}
-                            series={pieExpenseInvestmentParams.series.map((series) => ({
-                                ...series,
-                                highlightScope: {
-                                    highlighted,
-                                    faded,
-                                },
-                            }))}
-                            onClick={(data, index) => {
-                                //the pie knows their index but not data?
-                            }}
-                        />) : <>No Expense and Investment Data</>}
-                    </Box>
-                    <FormControl>
-                        <InputLabel id="demo-simple-select-label" >Select Year</InputLabel>
-                        <Select
-                            labelId="select-label"
-                            id="select-year"
-                            value={currentYear ? currentYear : ""}
-                            label="Select Year"
-                            onChange={(e) => {
-                                setCurrentYear(e.target.value)
-                            }}
-                        >
-                            {
-                                userData ? (
-                                    userData.map((data, index) =>
-                                        (<MenuItem value={data.year} key={index + data.year}>{data.year}</MenuItem>)
-                                    )
-                                ) : null
-                            }
-                        </Select>
-                    </FormControl>
-                </Stack>
+                    </div>
+                ) : (
+                    <>No Expense and Investment Data</>
+                )}
+                <FormControl>
+                    <InputLabel id="demo-simple-select-label">Select Year</InputLabel>
+                    <Select
+                        labelId="select-label"
+                        id="select-year"
+                        value={currentYear ? currentYear : ""}
+                        label="Select Year"
+                        onChange={(e) => {
+                            setCurrentYear(e.target.value);
+                        }}
+                    >
+                        {userData
+                            ? userData.map((data, index) => (
+                                <MenuItem value={data.year} key={index + data.year}>
+                                    {data.year}
+                                </MenuItem>
+                            ))
+                            : null}
+                    </Select>
+                </FormControl>
             </Box>
         </Container>
     );
