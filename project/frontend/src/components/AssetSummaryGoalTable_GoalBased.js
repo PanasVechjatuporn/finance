5b3058ffcf47956447d5ae2c0c460c6db105cc34
Chokchai213 <chokchai.bis@gmail.com>
2024-03-26 03:31:30 +0700

    Add table summary for each goal

---
+++ project/frontend/src/components/AssetSummaryGoalTable_GoalBased.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+    '&.dateCell': {
+        backgroundColor: theme.palette.common.white,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+export const AssetSummaryGoalTable = ({ selectedData }) => {
+    if (selectedData) {
+        return (
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: "100%" }} aria-label="customized table">
+                    <TableHead>
+                        <StyledTableRow>
+                            <StyledTableCell rowSpan={2}>วันที่ซื้อ&nbsp;(วว/ดด/ปป)</StyledTableCell>
+                            <StyledTableCell>ชื่อกองทุน</StyledTableCell>
+                            <StyledTableCell>จำนวนเงินที่ซื้อ&nbsp;(บาท)</StyledTableCell>
+                            <StyledTableCell>ราคาที่ซื้อ&nbsp;(บาท)</StyledTableCell>
+                            <StyledTableCell>จำนวนหน่วยที่ซื้อ</StyledTableCell>
+                        </StyledTableRow>
+                    </TableHead>
+                    <TableBody>
+                        {selectedData.assets.map((asset, index) => (
+                            <React.Fragment key={index}>
+                                <StyledTableRow>
+                                    <StyledTableCell rowSpan={asset.Funds.length} className="dateCell">
+                                        {asset.CreatedDate}
+                                    </StyledTableCell>
+                                    {asset.Funds.length > 0 &&
+                                        <React.Fragment>
+                                            <StyledTableCell>{asset.Funds[0].fundName}</StyledTableCell>
+                                            <StyledTableCell>{asset.Funds[0].amount}</StyledTableCell>
+                                            <StyledTableCell>{asset.Funds[0].buyPrice}</StyledTableCell>
+                                            <StyledTableCell>{asset.Funds[0].unit}</StyledTableCell>
+                                        </React.Fragment>
+                                    }
+                                </StyledTableRow>
+                                {asset.Funds.slice(1).map((fund, index) => (
+                                    <StyledTableRow key={index}>
+                                        <StyledTableCell>{fund.fundName}</StyledTableCell>
+                                        <StyledTableCell>{fund.amount}</StyledTableCell>
+                                        <StyledTableCell>{fund.buyPrice}</StyledTableCell>
+                                        <StyledTableCell>{fund.unit}</StyledTableCell>
+                                    </StyledTableRow>
+                                ))}
+                            </React.Fragment>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        )
+    }
+
+}
--- project/frontend/src/components/AssetSummary_GoalBased.js
+++ project/frontend/src/components/AssetSummary_GoalBased.js
@@ -1,58 +1,104 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 import { useSelector } from "react-redux";
+import { AssetSummaryGoalTable } from "./AssetSummaryGoalTable_GoalBased";
 import axios from "axios";
 
 const baseURL = "http://localhost:8000";
 
-
 async function getUserAssets(userStore) {
-        const getResult = await axios
-        .get(`${baseURL}/db/get_user_asset`, {
-            headers: {
-                Authorization: userStore.userToken,
-                userId: userStore.userId,
-            },
-        })
-        return getResult.data.queryResult
-}
-
-async function getUserGoal(userStore){
-    const getResult = await axios
-    .get(`${baseURL}/db/get_user_goal`, {
+    const getResult = await axios.get(`${baseURL}/db/get_user_asset`, {
         headers: {
             Authorization: userStore.userToken,
             userId: userStore.userId,
         },
-    })
-    return getResult.data.queryResult
+    });
+    return getResult.data.queryResult;
 }
 
-function combineGoalAndAsset(assetData, goalData){
-    console.log('assetData :: ',assetData)
-    console.log('goalData :: ',goalData)
-    return "GAY"
+async function getUserGoal(userStore) {
+    const getResult = await axios.get(`${baseURL}/db/get_user_goal`, {
+        headers: {
+            Authorization: userStore.userToken,
+            userId: userStore.userId,
+        },
+    });
+    return getResult.data.queryResult;
+}
+
+function combineGoalAndAsset(assetData, goalData) {
+    goalData.forEach((goal, index) => {
+        const thisGoalAssets = assetData.filter(
+            (asset) => asset.goalObjId === goal._id
+        );
+        goalData[index].assets = thisGoalAssets;
+    });
+    return goalData;
 }
 
 export const AssetSummary = () => {
     const userStore = useSelector((state) => state.userStore);
     const [userData, setUserData] = useState(null);
-    
+    const [selectGoalValue, setSelectGoalValue] = useState("");
+    const [selectedData, setSelectedData] = useState(null);
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const assetData = await getUserAssets(userStore);
                 const goalData = await getUserGoal(userStore);
-                const userData = combineGoalAndAsset(assetData,goalData);
+                const userData = combineGoalAndAsset(assetData, goalData);
+                if (userData.length > 0) {
+                    setSelectGoalValue(userData[0].Name);
+                }
                 setUserData(userData);
             } catch (error) {
-                console.error('Error fetching user assets:', error);
+                console.error("Error fetching user assets:", error);
             }
         };
         fetchData();
     }, [userStore]);
-    return (
-        <>
-            {JSON.stringify(userData)}
-        </>
-    )
-}
+
+    useEffect(() => {
+        if(userData){
+            setSelectedData(userData.find((data) => data.Name === selectGoalValue));
+        }
+    }, [selectGoalValue, userData]);
+    if (userData) {
+        return (
+            <Container sx={{ marginTop: 5, display: "flex", position: "relative" }}>
+                <Box sx={{ width: 140, position: "absolute", right: 0 }}>
+                    <FormControl sx={{ width: "100%" }}>
+                        <InputLabel id="select-goal-label">Goals</InputLabel>
+                        <Select
+                            labelId="select-goal"
+                            id="select-goal"
+                            value={selectGoalValue}
+                            label="Goals"
+                            onChange={(e) => {
+                                setSelectGoalValue(e.target.value);
+                            }}
+                        >
+                            {userData.map((data, index) => (
+                                <MenuItem key={data._id + "-select-item"} value={data.Name}>
+                                    {data.Name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Box>
+                    <AssetSummaryGoalTable
+                        selectedData={selectedData}
+                    ></AssetSummaryGoalTable>
+                </Box>
+            </Container>
+        );
+    } else {
+        return <></>;
+    }
+};
