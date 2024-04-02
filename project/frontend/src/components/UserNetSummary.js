11efce9860c708438945e9a2b8fe9f9c79584e1c
Chokchai213 <chokchai.bis@gmail.com>
2024-04-02 20:30:51 +0700

    add query usernetsummary

--- project/frontend/src/components/UserNetSummary.js
+++ project/frontend/src/components/UserNetSummary.js
@@ -4,10 +4,36 @@ import { useSelector } from "react-redux";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
+import axios from "axios";
+
+const baseURL = "http://localhost:8000";
+
+async function fetchUserNetSummary(userStore){
+    const getResult = await axios.get(`${baseURL}/db/get_user_net_summary`, {
+        headers: {
+            Authorization: userStore.userToken,
+            userId: userStore.userId,
+        },
+    });
+    console.log('getResult ::',getResult)
+    return getResult.data;
+}
 
 export const UserNetSummary = ({userData}) => {
     const userStore = useSelector((state) => state.userStore);
-    console.log('userData :: ',userData)
+    const [userNetSummary, setUserNetSummary] = useState(null);
+    useEffect(()=> {
+        console.log('userData change')
+        const fetchData = async () => {
+            try {
+                const userNetSummary = await fetchUserNetSummary(userStore);
+                setUserNetSummary(userNetSummary);
+            } catch (error) {
+                console.error("Error fetching user assets:", error);
+            }
+        };
+        fetchData()
+    },[userData, userStore])
     return (
         <Box>
             <Container>
@@ -28,6 +54,9 @@ export const UserNetSummary = ({userData}) => {
                         padding: 1,
                     }}
                 >ยอดเงินสะสมของคุณ</Typography>
+                <>
+                {userNetSummary && userNetSummary.netIncomeExpense}
+                </>
             </Container>
         </Box>
     )
