c5d443c0a1bc8ce704885dfe49ea11e36b44596c
PanasVechjatuporn <97963362+PanasVechjatuporn@users.noreply.github.com>
2024-04-23 22:46:18 +0700

    Footer update

--- project/frontend/src/components/Footer.js
+++ project/frontend/src/components/Footer.js
@@ -20,7 +20,6 @@ export const Footer = () => {
         <div className="container">
           <div style={{ display: "flex"}}>
             <div className="footer-col-logo">
-              <img src="https://mahidol.ac.th/documents/CI/update/Mahidol/1.png"></img>
             </div>
             {/* <div className="footer-col">
               <h4>Limitation of the program</h4>
@@ -39,14 +38,12 @@ export const Footer = () => {
             <div className="footer-col">
               <h4>follow us</h4>
               <div className="social-links">
-                <a href="https://github.com/Chokchai213/web-modules" target="_blank">
-                  <GitHubIcon />
-                </a>
+              
               </div>
             </div>
           </div>
           <div className="end-of-footer">
-            <h5>Asset of Mahidol University, 2024</h5>
+            <h5></h5>
           </div>
         </div>
       </Container>
